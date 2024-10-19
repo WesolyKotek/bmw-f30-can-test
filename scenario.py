ca7aa7cb15bb049715b1d7b28edb278f31326052
Alexey Lysachkov <lsch05@mail.ru>
2024-10-19 23:19:13 +0500

    wip v0.0.3

--- scenario.py
+++ scenario.py
@@ -2,6 +2,8 @@ import json
 import time
 import os
 
+import asyncio
+
 
 class Scenario:
     def __init__(self, filename, uds_client):
@@ -15,22 +17,33 @@ class Scenario:
         with open(self.filename, 'r') as file:
             return json.load(file)
 
-    def run(self):
+    async def run(self):
         self.running = True
         repeats = self.scenario.get('repeats', 1)
         messages = self.scenario['messages']
+
+        start_check_message = self.scenario['start_check_message']
+        start_sequence = self.scenario['start_check_message_responses']
+        stop_check_message = self.scenario['stop_check_message']
+        stop_check_sequence = self.scenario['stop_check_message_responses']
+        start_stop_responses_timeout = self.scenario.get('start_stop_responses_timeout', 1)
+
+        if start_sequence:
+            await self.check_for_sequence(start_check_message, start_sequence,
+                                          stop_check_message, stop_check_sequence,
+                                          start_stop_responses_timeout)
 
         repeat_count = 0
         while self.running:
             repeat_count += 1
             print(f'{repeat_count}/inf' if repeats == -1 else f'{repeat_count}/{repeats}')
 
-            self.send_uds_message(messages)
+            await self.send_uds_message(messages)
 
             if repeats != -1 and repeat_count >= repeats:
                 break
 
-    def send_uds_message(self, messages):
+    async def send_uds_message(self, messages):
         for message in messages:
             if not self.running:
                 print('Scenario stopped')
@@ -39,6 +52,46 @@ class Scenario:
             delay = message['delay'] / 1000
             self.uds_client.send(data)
             time.sleep(delay)
+
+    async def check_for_sequence(self,
+                                 start_check_message, start_sequence,
+                                 stop_check_message, stop_sequence,
+                                 start_stop_responses_timeout):
+
+        received_packets = []
+        start_time = time.time()
+        while not self.stop_event.is_set():
+            try:
+                packet = await asyncio.wait_for(self.uds_client.receive(),
+                                                timeout=start_stop_responses_timeout)
+
+                if packet:
+                    print(f"Получен пакет: {packet}")
+
+                    # Если пакет совпадает с ожидаемым элементом последовательности
+                    if packet == sequence[0]:
+                        received_packets.append(packet)
+                        sequence.pop(0)
+                        print(f"Найден правильный пакет, оставшиеся для получения: {len(sequence)}")
+
+                        # Если вся последовательность найдена
+                        if not sequence:
+                            print("Последовательность пакетов успешно найдена!")
+                            return True
+                        else:
+                            print(f"Неправильный пакет: {packet}, выкидываем и продолжаем искать.")
+                    else:
+                        print("Буфер пустой, ждем нового пакета.")
+
+                except asyncio.TimeoutError:
+                    print("Время ожидания пакета истекло.")
+                    # Сбрасываем прогресс поиска последовательности и начинаем заново
+                    received_packets.clear()
+                    sequence = self.check_sequence.copy()
+
+                    print("Последовательность не найдена.")
+                    return False
+
 
     def stop(self):
         self.running = False
